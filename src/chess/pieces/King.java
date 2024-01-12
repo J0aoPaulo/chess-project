a87f1fea9e1055d34d5a73e9d918ed4ea853dff8
João Paulo Almeida <joao.almeida@alu.ufc.br>
2024-01-12 19:14:07 -0300

    add special move Castling

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,12 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
-    public King(Board board, Color color) {
+
+    private ChessMatch chessMatch;
+
+    public King(Board board, Color color, ChessMatch chessMatch) {
         super(board, color);
+        this.chessMatch = chessMatch;
     }
 
     @Override
@@ -23,6 +28,42 @@ public class King extends ChessPiece {
     private void verifyMove(Position p, boolean[][] mat) {
         if (getBoard().positionExists(p.getRow(), p.getColumn()) && canMove(p)) {
             mat[p.getRow()][p.getColumn()] = true;
+        }
+    }
+
+    private boolean verifyNullPosition(Position p) {
+        return getBoard().piece(p.getRow(), p.getColumn()) == null;
+    }
+
+    private boolean testRookCastling(Position position) {
+        ChessPiece p = (ChessPiece)getBoard().piece(position.getRow(), position.getColumn());
+        return p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
+    }
+
+    private void castlingRightSide(boolean [][] mat) {
+        if(getMoveCount() == 0 && !chessMatch.getCheck()) {
+            Position postT1 = new Position(position.getRow(), position.getColumn() + 3);
+            if(testRookCastling(postT1)) {
+                Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+                Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+                if(verifyNullPosition(p1) && verifyNullPosition(p2)) {
+                    mat[position.getRow()][position.getColumn() + 2] = true;
+                }
+            }
+        }
+    }
+
+    private void castlingLeftSide(boolean[][] mat) {
+        if(getMoveCount() == 0 && !chessMatch.getCheck()) {
+            Position postT2 = new Position(position.getRow(), position.getColumn() - 4);
+            if(testRookCastling(postT2)) {
+                Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+                Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+                Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+                if(verifyNullPosition(p1) && verifyNullPosition(p2) && verifyNullPosition(p3)) {
+                    mat[position.getRow()][position.getColumn() - 2] = true;
+                }
+            }
         }
     }
 
@@ -61,6 +102,10 @@ public class King extends ChessPiece {
         // se
         p.setValues(position.getRow() + 1, position.getColumn() + 1);
         verifyMove(p, mat);
+
+        // Special moves
+        castlingRightSide(mat);
+        castlingLeftSide(mat);
 
         return mat;
     }
